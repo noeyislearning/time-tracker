6efcb60177ffcf312040a896c50507959ef0de35
Ibrahim Lincoln <noeyislearning@gmail.com>
2023-11-30 00:11:40 +0800

    docs: add documentation on projects application

--- server/apps/api/projects/urls.py
+++ server/apps/api/projects/urls.py
@@ -7,12 +7,23 @@ from .views import (
     ProjectTracksList
 )
 
+# Application namespace
 app_name = "projects"
 
+
 urlpatterns = [
+    # Root API Route
     path('', ProjectList.as_view(), name='list'),
+
+    # Create API Route
     path('c/', ProjectCreate.as_view(), name='create'),
+
+    # Retrieve API Route
     path('r/<pk>/', ProjectRetrieve.as_view(), name='retrieve'),
+
+    # Retieve & Update API Route
     path('rud/<pk>/', ProjectRetrieveUpdateDestroy.as_view(), name='retrieve_update_destroy'),
+
+    # Retrieve with Tracks API Route
     path('rwt/<pk>/', ProjectTracksList.as_view(), name='retrieve_with_tracks'),
 ]
